f3f6471fdc60acc1ab7b5ee461f6a150137a4f01
Jhonatan <jhon20339@gmail.com>
2019-09-23 19:01:20 -0500

    Correccion Capitulo 1 - Ejercicio 7

--- Capitulo_01/src/Ejercicio_07.java
+++ Capitulo_01/src/Ejercicio_07.java
@@ -5,7 +5,7 @@ public class Ejercicio_07 {
 		// TODO Auto-generated method stub
 		
 		
-		System.out.println(4*(1-(1/3)+(1/5)-(1/7)+(1/9)-(1/11)+(1/13)));
+		System.out.println(4.0*(1.0-(1.0/3.0)+(1.0/5.0)-(1.0/7.0)+(1.0/9.0)-(1.0/11.0)+(1.0/13.0)));
 	}
 
 }
