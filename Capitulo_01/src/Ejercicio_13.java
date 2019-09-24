053f6e8543f2f379685fd2aab95cd4bd44214457
Jhonatan <jhon20339@gmail.com>
2019-09-23 19:23:32 -0500

    Ejercicio 13 capitulo 1

---
+++ Capitulo_01/src/Ejercicio_13.java
@@ -0,0 +1,20 @@
+
+public class Ejercicio_13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double A = 3.4;
+		double B = 50.2;
+		double C = 2.1;
+		double D = .55;
+		double E = 44.5;
+		double F = 5.9;
+		double X = (E*D-B*F)/(A*D-B*C);
+		double Y = (A*F-E*C)/(A*D-B*C);
+		System.out.println(3.4*X+50.2*Y);
+		System.out.println(2.1*X+.55*Y);
+		
+
+	}
+
+}
